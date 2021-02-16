e710241455ca464f0e39b1773638c50436241abf
MatteoGioioso <gioioso.matteo@gmail.com>
2021-02-16 09:52:32 +0700

    add test with expressjs

---
+++ __tests__/expressjs/server.js
@@ -0,0 +1,99 @@
+const http = require("http");
+const {Sequelize} = require('sequelize')
+const Sentry = require('@sentry/node')
+const cors = require('cors')
+// This package must be imported even if there are no methods to require
+const {expressMiddleware, expressErrorHandler} = require('../agent-nodejs');
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const port = 9000
+
+Sentry.init({
+    dsn: "https://9cd1dff7aff84daeb2bdf110e9aa8d80@o260622.ingest.sentry.io/5636379",
+});
+
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5433/postgres') // Example for postgres
+const getDataFromDatabase = async () => {
+    const res = await sequelize.query('SELECT 1 + 5 * 3 AS result')
+    console.log("Outside wrapper", res)
+    return res[0][0].result
+}
+
+const callExternalAPI = async () => {
+    const options = {
+        host: 'jsonplaceholder.typicode.com',
+        path: '/todos/1',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = http.request(options, (res) => {
+            console.log(`${options.host} : ${res.statusCode}`);
+            res.setEncoding('utf8');
+            let output = '';
+
+            res.on('data', (chunk) => {
+                output += chunk;
+            });
+
+            res.on('end', () => {
+                let obj = JSON.parse(output);
+                resolve(obj);
+            });
+        });
+
+        req.on('error', (err) => {
+            reject(err)
+        });
+
+        req.end();
+    })
+}
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(Sentry.Handlers.requestHandler());
+app.use(expressMiddleware({}))
+app.post('/', async (req, res, next) => {
+    const {num} = req.body
+    console.log("Received number: ", num)
+
+    const data = await callExternalAPI();
+    console.log(data)
+
+    // const moreData = await getDataFromDatabase()
+    // console.log(moreData)
+
+    console.log("Title: ", data.title)
+
+    const length = data.title.length;
+    console.log("Title length: ", length)
+
+    if (length - num === 0){
+        return next(new Error("Error: division by 0!!"))
+    }
+
+    const magicNumber =  18 / (length - num)
+
+    res.status(200).send({
+        magicNumber
+    })
+})
+
+app.use(expressErrorHandler({Sentry}))
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.send(err.message)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
